ba3c1a31d6467bfeb6ed924b850e6504e543dd5b
kirandhongadi7 <kirandhongadi7799@gmail.com>
2025-02-08 21:17:56 +0530

    This is working log

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const arr = [1,2,3,3,4]
-console.log(arr);
+const arrs = [1,2,3,3,4]
+console.log(arrs);
 const nums = [12,3,4,5]
 console.log(nums);
 
+console.log("This is working!");
--- two.js
+++ two.js
@@ -2,4 +2,9 @@ function add(a,b){
     console.log(a+b);
     
 }
+function sub(a,b){
+    console.log(a-b);
+    
+}
 add(7,8)
+sub(9,3)
